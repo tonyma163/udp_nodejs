cb1f078cbcc79bc17dc1f95dda973e1b343d1c59
unknown <mamanto163@gmail.com>
2023-10-30 20:59:07 +0800

    bug_cannot_show_received_message

--- app/client.js
+++ app/client.js
@@ -6,13 +6,41 @@ const prompt = require('prompt-sync')();
 //const msg = Buffer.from('test');
 const client = dgram.createSocket('udp4');
 
-client.connect(3000, 'localhost', (err) => {
-    var exit = 0;
-    while (exit == 0) {
+const roomId = "room1";
+
+// receive message from another clients through the server
+client.on("message", (msg, _sender) => {
+    console.log("receive.");
+
+    console.log(`Recevied message from: ${_sender.address}:${_sender.port}: ${msg}`);
+})
+
+client.connect(3000, 'localhost', async(err) => {
+    if (err) {
+        console.log('Error connecting to the server: ', err);
+        return;
+    }
+
+    // send msg
+    client.send(roomId+" join", (err) => {
+        if (err) {
+            console.err("Error sending message: ", err);
+        }
+    })
+
+    while (true) {
         let msg = prompt("Enter ur message: ");
-        // send msg
-        client.send(msg, (err) => {
+        
+        if (msg == "exit") {
             client.close();
+            break;
+        }
+
+        // send msg
+        client.send(roomId+" "+msg, (err) => {
+            if (err) {
+                console.err("Error sending message: ", err);
+            }
         })
-}
+    }
 })
--- app/server.js
+++ app/server.js
@@ -1,13 +1,47 @@
 const dgram = require('node:dgram');
 const server = dgram.createSocket('udp4');
 
+const clients = {}; // Store connected clients by room
+
 server.on('error', (err) => {
     console.log('Server error: ', err.stack);
     server.close();
 })
 
 server.on('message', (_msg, _sender) => {
-    console.log(`Received from ${_sender.address}: ${_sender.port}: ${_msg}`);
+    console.log(`Received from ${_sender.address}:${_sender.port}: ${_msg}`);
+
+    const msg = _msg.toString();
+    const [roomId, content] = msg.split(" ");
+    
+    // Create a new room if it doesn't exist
+    if (!(roomId in clients)) {
+        clients[roomId] = [];
+    }
+
+    // Check if the sender is already in the room; if not, add them
+    const senderKey = `${_sender.address}:${_sender.port}`;
+    if (!clients[roomId].some(client => client === senderKey)) {
+        clients[roomId].push(senderKey);
+    }
+    console.log("clients: ",clients)
+    if (roomId in clients) {
+        clients[roomId].forEach(client => {
+            // Split the client string into address and port
+            const [clientAddress, clientPort] = client.split(':');
+            console.log("client Address: ", clientAddress);
+            console.log("client Port: ",clientPort);
+            // Check port and address to find the recipient
+            if (parseInt(clientPort) !== _sender.port) {
+                server.send(_msg, parseInt(clientPort), clientAddress, (err) => {
+                    if (err) {
+                        console.error(`Error sending message to ${clientAddress}:${clientPort}`);
+                    }
+                })
+                console.log("sent")
+            }
+        })
+    }
 })
 
 server.on('listening', () => {
